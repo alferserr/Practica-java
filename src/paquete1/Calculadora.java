2a69ebc6b70616355961f25fa8cd248f5d4c5505
alferserr <alferserr@hotmail.com>
2022-07-30 19:38:56 +0200

    Nada especial

---
+++ src/paquete1/Calculadora.java
@@ -0,0 +1,9 @@
+package paquete1;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner escaner = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        int num = escaner.nextInt();
+    }
+}
---
+++ src/paquete1/OperacionesCalculadora.java
@@ -0,0 +1,2 @@
+package paquete1;public class OperacionesCalculadora {
+}
