a105315bee1c5ee43138f01dbc488e4d02f54833
alferserr <alferserr@hotmail.com>
2022-07-30 19:40:45 +0200

    Calculadora

--- src/paquete1/Calculadora.java
+++ src/paquete1/Calculadora.java
@@ -1,9 +1,44 @@
 package paquete1;
 import java.util.Scanner;
-public class Main {
+
+public class Calculadora {
     public static void main(String[] args) {
-        Scanner escaner = new Scanner(System.in);
-        System.out.println("Introduce un numero");
-        int num = escaner.nextInt();
+        Scanner escaner=new Scanner(System.in);
+        OperacionesCalculadora oc = new OperacionesCalculadora();
+
+        System.out.println("Que operacion quieres hacer? (Introduce abajo el numero segun el tipo de operacion que quieras hacer)");
+        System.out.println("1) Suma");
+        System.out.println("2) resta");
+        System.out.println("3) multiplicacion");
+        System.out.println("4) division");
+
+        int resul = escaner.nextInt();
+
+        switch (resul){
+            case 1:
+                oc.datos();
+                oc.suma();
+                break;
+            case 2:
+                oc.datos();
+                oc.resta();
+                break;
+            case 3:
+                oc.datos();
+                oc.multiplicacion();
+                break;
+            case 4:
+                oc.datos();
+                oc.division();
+                break;
+            case 5:
+                System.exit(0);
+
+        }
+
     }
+
+
+
+
 }
--- src/paquete1/OperacionesCalculadora.java
+++ src/paquete1/OperacionesCalculadora.java
@@ -1,2 +1,36 @@
-package paquete1;public class OperacionesCalculadora {
+package paquete1;
+
+import java.util.Scanner;
+
+public class OperacionesCalculadora {
+    Scanner escaner = new Scanner(System.in);
+    int num1,num2,suma,resta,multiplicacion,division;
+
+    public void suma(){
+        suma=num1+num2;
+        System.out.println("El resultado de la suma es: "+suma);
+    }
+
+    public void resta(){
+        resta=num1-num2;
+        System.out.println("El resultado de la resta es: "+resta);
+    }
+
+    public void multiplicacion(){
+        multiplicacion=num1*num2;
+        System.out.println("El resultado de la multiplicacion es "+multiplicacion);
+    }
+
+    public void division(){
+        division=num1/num2;
+        System.out.println("El resultado de la division es "+division);
+    }
+
+    public void datos(){
+        System.out.println("Ingresa el primer numero");
+        num1 = escaner.nextInt();
+        System.out.println("Ingresa el segundo numero");
+        num2 = escaner.nextInt();
+    }
+
 }
